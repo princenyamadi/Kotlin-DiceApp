6deba5fae66c1cbf77f64b85bf07774350be4296
princenyamadi <princenyamadi.pn@gmail.com>
2021-04-20 00:02:34 +0000

    refactor: employed find views efficiency method(nullable, lateinit var)

--- app/src/main/java/tech/peny/diceapp/MainActivity.kt
+++ app/src/main/java/tech/peny/diceapp/MainActivity.kt
@@ -2,10 +2,32 @@ package tech.peny.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var randomNumberTxt : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        randomNumberTxt = findViewById<TextView>(R.id.text_view_random_number)
+
+        val rollButton = findViewById<Button>(R.id.btn_roll_dice);
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+
+
     }
+
+    fun rollDice(){
+        val randomNumber = (1..6).random()
+//        val randomNumberTxt = findViewById<TextView>(R.id.text_view_random_number)
+        randomNumberTxt.text = randomNumber.toString()
+    }
+
+
+
 }
